a113e225d14ec162908c51fd93ebe0246bc12608
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-01-27 17:23:26 +0600

    cart -done

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Cart = () => {
     return (
-        <div>
-            cart
+        <div className='pl-5 mt-5'>
+            <h3 className='text-2xl font-bold text-center mb-9'>Order Summary</h3>
+            <p>Selected Item : </p>
+            <p>Total Price :</p>
+            <p>Total Shiping: </p>
+            <p>Tax + Vat : </p>
+            <p className='text-lg font-semibold'>Grand Total : </p>
         </div>
     );
 }
